ab8dbab4e749f911aad4aa4d355574d785047763
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-04-17 21:43:50 +0100

    Add Arp button.

--- src/uk/co/tekkies/akko/MainActivity.java
+++ src/uk/co/tekkies/akko/MainActivity.java
@@ -3,13 +3,17 @@ package uk.co.tekkies.akko;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.textViewArp).setOnClickListener(this);
     }
 
 
@@ -19,5 +23,23 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()) {
+		case R.id.textViewArp:
+			onClickTextViewArp();
+			break;
+		}
+	}
+
+
+	private void onClickTextViewArp() {
+		Toast.makeText(this, "Arp pressed", Toast.LENGTH_SHORT).show();
+		// TODO Auto-generated method stub
+		
+	}
     
 }
