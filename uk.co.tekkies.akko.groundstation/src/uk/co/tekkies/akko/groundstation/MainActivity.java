efa5c77f0e7a512cadd56749a442b0e7aea87835
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-05-04 23:09:10 +0100

    Groundstation send UDP packet to onboard running on localhost.

--- uk.co.tekkies.akko.groundstation/src/uk/co/tekkies/akko/groundstation/MainActivity.java
+++ uk.co.tekkies.akko.groundstation/src/uk/co/tekkies/akko/groundstation/MainActivity.java
@@ -1,5 +1,13 @@
 package uk.co.tekkies.akko.groundstation;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -10,6 +18,43 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		new UdpAsyncTask().execute(0);
+		new UdpAsyncTask().execute(0);
+	}
+	
+	
+	
+    class UdpAsyncTask extends AsyncTask<Integer, String, String> {
+
+		private static final int MAX_UDP_DATAGRAM_LEN = 1024;
+
+		@Override
+		protected String doInBackground(Integer... params) {
+			sendUdpPacket();
+	        return null;
+	    }
+
+    }	
+
+	private void sendUdpPacket() {
+		String messageStr="Message from groundstation";
+		int server_port = 1985;
+		DatagramSocket s=null;
+		InetAddress local=null;
+		try {
+			s = new DatagramSocket();
+			local = InetAddress.getByName("localhost");
+			int msg_length=messageStr.length();
+			byte[] message = messageStr.getBytes();
+			DatagramPacket p = new DatagramPacket(message, msg_length,local,server_port);
+			s.send(p);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (SocketException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
 	}
 
 	@Override
