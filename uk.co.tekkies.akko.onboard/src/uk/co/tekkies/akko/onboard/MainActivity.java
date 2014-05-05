36ad08fd9e0d8429da77f10aae79f87c05d8f1da
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-05-05 22:10:28 +0100

    Test: Distance calculation.

--- uk.co.tekkies.akko.onboard/src/uk/co/tekkies/akko/onboard/MainActivity.java
+++ uk.co.tekkies.akko.onboard/src/uk/co/tekkies/akko/onboard/MainActivity.java
@@ -60,6 +60,11 @@ public class MainActivity extends Activity implements OnClickListener, DroneStat
         
         //onClickTextViewDrone();
         
+        
+        calculateDistance(51.5728, -4.087, 51.5739, -4.0877);
+        
+        
+        
         new UdpAsyncTask().execute(1985);
 
     }
@@ -303,6 +308,24 @@ public class MainActivity extends Activity implements OnClickListener, DroneStat
 	
 	
 
+    public final static double AVERAGE_RADIUS_OF_EARTH = 6371;
+    public int calculateDistance(double userLat, double userLng, double venueLat, double venueLng) {
+
+        double latDistance = Math.toRadians(userLat - venueLat);
+        double lngDistance = Math.toRadians(userLng - venueLng);
+
+        double a = (Math.sin(latDistance / 2) * Math.sin(latDistance / 2)) +
+                        (Math.cos(Math.toRadians(userLat))) *
+                        (Math.cos(Math.toRadians(venueLat))) *
+                        (Math.sin(lngDistance / 2)) *
+                        (Math.sin(lngDistance / 2));
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        double km = AVERAGE_RADIUS_OF_EARTH * c;
+        int m = (int)(km*1000);
+        return m;
+    }
 
 	
 }
