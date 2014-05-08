e31c4be40043fb5752e0846fae8ea462d08a8d08
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-05-08 23:02:37 +0100

    Track LHS joystick down, down point and up.

--- uk.co.tekkies.akko.groundstation/src/uk/co/tekkies/akko/groundstation/JoystickView.java
+++ uk.co.tekkies.akko.groundstation/src/uk/co/tekkies/akko/groundstation/JoystickView.java
@@ -5,6 +5,7 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -13,6 +14,7 @@ public class JoystickView extends View {
 	Paint bluePaint;
 	MotionEvent motionEvent=null;
 	Point origin;
+	Point lhsDown=null;
 
 	public JoystickView(Context context) {
 		super(context);
@@ -45,13 +47,8 @@ public class JoystickView extends View {
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		calculateOrigin();
-		if(motionEvent != null) {
-			for(int i=0;i<motionEvent.getPointerCount();i++) {
-				Paint paint = ((motionEvent.getX(i)-origin.x) < (getWidth()/2)) ? redPaint : bluePaint; 
-				Rect touchRect = new Rect(0,0,(int)motionEvent.getX(i)-origin.x, (int)motionEvent.getY(i)-origin.y);
-				canvas.drawRect(touchRect, paint);
-			}
-		}
+		//TODO: Move call to onTouchEvent - only needs recalculating on touch. 
+		calculateJoystickPositions(motionEvent, canvas);
 		if(isInEditMode()) {
 			canvas.drawRect(new Rect(50,50,150,150), redPaint);
 		}
@@ -64,4 +61,51 @@ public class JoystickView extends View {
 		invalidate();
 		return true;
 	}
+	int leftPointerId=-2;
+	private void calculateJoystickPositions(MotionEvent motionEvent, Canvas canvas) {
+		if(motionEvent != null) {
+			//LHS
+			int leftPointerIndex = motionEvent.findPointerIndex(leftPointerId);
+			if(motionEvent.getActionMasked()==MotionEvent.ACTION_UP && motionEvent.getActionIndex() == leftPointerIndex) {
+				//LHS UP
+				//TODO: This is still iffy.
+				Log.i("ACTION", "LHS UP");
+				leftPointerId = -2;
+				lhsDown = null;
+			} else {
+				if(leftPointerIndex < 0) {
+					//Find a pointer on LHS of screen.
+					for(int i=0;i<motionEvent.getPointerCount();i++) {
+						//on LHS of screen?
+						if((motionEvent.getX(i)-origin.x) < (getWidth()/2)) {
+							leftPointerIndex = i;
+							leftPointerId = motionEvent.getPointerId(leftPointerIndex);
+							lhsDown = new Point((int)motionEvent.getX(leftPointerIndex)-origin.x, (int)motionEvent.getY(leftPointerIndex)-origin.y);
+							break;
+						}
+					}
+				}
+				
+				if(leftPointerIndex >= 0) {
+					Rect touchRect = new Rect(lhsDown.x, lhsDown.y, (int)motionEvent.getX(leftPointerIndex)-origin.x, (int)motionEvent.getY(leftPointerIndex)-origin.y);
+					canvas.drawRect(touchRect, redPaint);
+				}
+			}
+			//RHS
+		
+
+			
+			//Debug
+			for(int i=0;i<motionEvent.getPointerCount();i++) {
+				float x = (motionEvent.getHistorySize() > 0) ? motionEvent.getHistoricalAxisValue(MotionEvent.AXIS_X, i, 0) : motionEvent.getX(i);
+				Paint paint = ((x-origin.x) < (getWidth()/2)) ? redPaint : bluePaint; 
+				Rect touchRect = new Rect(0,0,(int)motionEvent.getX(i)-origin.x, (int)motionEvent.getY(i)-origin.y);
+				canvas.drawRect(touchRect, paint);
+			}
+		} else {
+			lhsDown = null;
+		}
+		
+	}
 }
+
