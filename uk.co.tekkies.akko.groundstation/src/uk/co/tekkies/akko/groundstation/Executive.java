56c64b7990a22ba148e781757331f67754279204
Andy Joiner <andy.joiner@tekkies.co.uk>
2014-05-11 22:38:44 +0100

    WIP: Find onboard android on LAN by broadcast before entering main loop.

--- uk.co.tekkies.akko.groundstation/src/uk/co/tekkies/akko/groundstation/Executive.java
+++ uk.co.tekkies.akko.groundstation/src/uk/co/tekkies/akko/groundstation/Executive.java
@@ -19,12 +19,16 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 public class Executive {
-	final int MAX_THREADS=3;
+	private static final int MAX_UDP_DATAGRAM_LEN = 1024;
+	private static final int MAX_THREADS=3;
+	private static final int ONBOARD_PORT = 1985;
+	private static final int GROUNDSTATION_PORT = 1986;
 	
 	private volatile boolean keepRunning = true;
 	private ThreadPoolExecutor threadPoolExecutor;
 	private MainActivity mainActivity=null;
 	int session = 0;
+	private volatile InetAddress inetAddressOnboard=null;
 
 	public Executive() {
 		threadPoolExecutor = new ThreadPoolExecutor(
@@ -39,7 +43,7 @@ public class Executive {
 	public void start(MainActivity mainActivity)	{
 		this.mainActivity = mainActivity;
 		keepRunning = true;
-        new UdpReceiveAsyncTask().executeOnExecutor(threadPoolExecutor, 1986);
+        new UdpReceiveAsyncTask().executeOnExecutor(threadPoolExecutor, GROUNDSTATION_PORT);
 		new UdpSendAsyncTask().executeOnExecutor(threadPoolExecutor, 0);
 	}
 	
@@ -66,8 +70,16 @@ public class Executive {
 	        return null;
 	    }
 
-		private void connectAndRun() {
+		private void connectAndRun() throws IOException {
+    		datagramSocketSend = new DatagramSocket();
 			pingOnboard();
+			if(inetAddressOnboard != null) {
+				Log.i("COMMS","Connected"+inetAddressOnboard.toString());
+				mainLoop();
+			}
+		}
+
+		private void mainLoop() {
 			while(keepRunning) {
 				mainActivity.getJoystickView().getLhsStick(lhsJoystick);
 				//sendUdpPacket();
@@ -78,45 +90,30 @@ public class Executive {
 				}
 			}
 		}
-
 		
-		private void pingOnboard() {
+		private void pingOnboard() throws IOException {
+			inetAddressOnboard = null;
 			session = 1+(int) (Math.random()*1000000);
-			
-			
-			try {
-				sendUdpPacket(InetAddress.getByName("255.255.255.255"));
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			send("command=ping|session="+session, InetAddress.getByName("255.255.255.255"));
+			int loops = 20;
+			while(inetAddressOnboard == null && loops > 0) {
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				loops--;
 			}
-
-			
-			
-//			long[] range = getSubnetRange();
-//			for(int ipInt=range[0];ipInt !> range[1]; ipInt++) {
-//				byte[] ipBytes = BigInteger.valueOf(ipInt).toByteArray();
-//				try {
-//					sendUdpPacket(InetAddress.getByAddress(ipBytes));
-//				} catch (UnknownHostException e) {
-//					e.printStackTrace();
-//				}
-//			}
 		}
-		private int[] getSubnetRange() {
-			WifiManager wifiManager= (WifiManager) mainActivity.getSystemService(Context.WIFI_SERVICE);
-			DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
-			int[] range = new int[2];  
-			range[0] = dhcpInfo.ipAddress & dhcpInfo.netmask;
-			range[1] = (dhcpInfo.ipAddress | ~dhcpInfo.netmask) & 0x8fffffff;
-			return range;
+
+		private void send(String message, InetAddress inetAddress) throws IOException {
+			datagramSocketSend.send(new DatagramPacket(message.getBytes(), message.length(),inetAddress,ONBOARD_PORT));
 		}
 
 		private void sendUdpPacket(InetAddress inetAddress) {
 			String messageStr="lhsX="+lhsJoystick.x;
 			int server_port = 1985;
 			try {
-	    		datagramSocketSend = new DatagramSocket();
 				int msg_length=messageStr.length();
 				byte[] message = messageStr.getBytes();
 				DatagramPacket p = new DatagramPacket(message, msg_length,inetAddress,server_port);
@@ -134,7 +131,6 @@ public class Executive {
 	
     class UdpReceiveAsyncTask extends AsyncTask<Integer, String, String> {
 
-		private static final int MAX_UDP_DATAGRAM_LEN = 1024;
 
 		@Override
 		protected String doInBackground(Integer... params) {
@@ -147,6 +143,7 @@ public class Executive {
 				socket = new DatagramSocket(params[0]);
 		        while(keepRunning) {
 	                socket.receive(packet);
+	                inetAddressOnboard = packet.getAddress();
 	                message = new String(lmessage, 0, packet.getLength());
 	                this.publishProgress(message);
 	                if(message.equalsIgnoreCase("exit")){
